35a30fe20505cb2b20a25fe3d4b66592515e56f2
Josh Gillies <github@joshgilli.es>
2015-04-14 15:30:10 +1000

    Update project to use `tito-webhook` module
    Replaces app/server.js

--- app/index.js
+++ app/index.js
@@ -1,8 +1,17 @@
+var webhook = require('tito-webhook')
 var debug = require('debug')('app')
 var http = require('http')
-var server = require('./server')
 
-var app = http.createServer(server)
+var _webhook = webhook('/', function(err, data) {
+  console.log(err || JSON.stringify(data))
+})
+
+var app = http.createServer(function server (req, res) {
+  _webhook(req, res, function done () {
+    res.statusCode = 404
+    res.end()
+  })
+})
 
 app.listen(process.env.PORT || 1337, process.env.HOST || '0.0.0.0', function listening () {
   var info = app.address()
